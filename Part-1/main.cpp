583761863e2c2559a478504797d774be6ee16ae7
pwong34 <pwong34@student.mtsac.edu>
2017-05-23 21:50:32 -0700

    Add files via upload

--- Part-1/main.cpp
+++ Part-1/main.cpp
@@ -2,19 +2,51 @@
 #include <string>
 #include "hashDictionary.h"
 
+void printMenu();
+void Option(hashDictionary<std::string, int>* );
 
-int main()
-{
-	hashDictionary<std::string, int> a(100);
-	a.insert("apple", 2);
-	a.insert("apple", 3);
-	a.insert("apple1", 4);
-	std::cout << a.find("apple1") << std::endl;
-	a.print();
-	a.remove("apple1");
-	a.print();
-	a.clear();
-	a.print();
-	hashDictionary<std::string, int> b(100);
+int main() {
+	int n;
+	std::cout << "Enter hash table size: ";
+	std::cin >> n;
+	hashDictionary<std::string, int>* hash = new hashDictionary<std::string, int>(n);
+	Option(hash);
 }
 
+void Option(hashDictionary<std::string, int>* hash) {
+	int option = 1;
+	std::string key;
+	int value;
+	while (option != 0) {
+		printMenu();
+		std::cin >> option;
+		switch (option) {
+		case 1:
+			std::cout << "Enter Key: ";
+			std::cin >> key;
+			std::cout << "Enter value: ";
+			std::cin >> value;
+			hash->insert(key, value);
+			break;
+		case 2:
+			std::cout << "Enter Key: ";
+			std::cin >> key;
+			value = hash->remove(key);
+			if (value != NULL) {
+				std::cout << "Record removed: " << value << std::endl;
+			} 
+			break;
+		case 3:
+			hash->print();
+			break;
+		case 4:
+			hash->clear();
+			break;
+		default:
+			break;
+		}
+	}
+}
+void printMenu() {
+	std::cout << "Output options (1 - insert, 2 - remove, 3 - print, 4 - clear)" << std::endl;
+}
